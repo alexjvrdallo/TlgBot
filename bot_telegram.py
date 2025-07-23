e313343e2ba295289e29c0c125b9efcfb2ed01a9
alexjvrdallo <alexjvrdallo@gmail.com>
2025-07-23 14:09:50 -0400

    Add files via upload

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
@@ -7,22 +8,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
-GROUP_ID = int(os.getenv("GROUP_ID"))
+GROUP_ID = os.getenv("GROUP_ID")
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start", "hola"])
-async def start_command(message: Message):
-    texto = f"👋 Bienvenido/a, {message.from_user.full_name}!"
-    await message.reply(texto)
+# /start handler
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: Message):
+    await message.reply(f"👋 Bienvenido/a, {message.from_user.full_name}!")
 
+# /ayuda handler
 @dp.message_handler(commands=["ayuda"])
-async def ayuda_command(message: Message):
-    texto = f"🚨 Solicitud de ayuda enviada."
+async def ayuda_handler(message: Message):
+    texto = "🚨 Solicitud de ayuda enviada."
+    await bot.send_message(chat_id=GROUP_ID, text=f"🚨 El usuario @{message.from_user.username} ha solicitado ayuda.")
     await message.reply(texto)
-    await bot.send_message(GROUP_ID, f"📢 El usuario {message.from_user.full_name} ha solicitado ayuda.")
 
 if __name__ == "__main__":
-    from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
